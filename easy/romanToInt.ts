81dbcaac9a6bd1cb5ec464253dad777a0d5d5bdf
Mikhail Molchanov <mikhail.molchanov@gmail.com>
2023-04-06 15:05:26 +0300

    romanToInt() problem: comments

--- easy/romanToInt.ts
+++ easy/romanToInt.ts
@@ -2,7 +2,21 @@
  * Task description:
  * https://leetcode.com/problems/roman-to-integer/
  */
-const symbols: Record<string, { value: number; inverse?: string[] }> = {
+
+type RomanSymbol = 'I' | 'V' | 'X' | 'L' | 'C' | 'D' | 'M';
+type Replacement = {
+  /**
+   * A number to add to the result when symbol is found.
+   */
+  value: number;
+  /**
+   * What symbols if present right before the current one will inverse its "value" to the negative.
+   * E.g. if I appeared after V (going right to left) then it should be subtracted from the result.
+   */
+  inverse?: readonly string[];
+};
+
+const replacements: Record<RomanSymbol, Replacement> = {
   I: { value: 1, inverse: ['V', 'X'] },
   V: { value: 5 },
   X: { value: 10, inverse: ['L', 'C'] },
@@ -14,18 +28,25 @@ const symbols: Record<string, { value: number; inverse?: string[] }> = {
 
 export const romanToInt = (s: string): number => {
   let result = 0;
-  let prev = '';
-  for (let i = s.length - 1; i >= 0; i--) {
-    const c = s[i];
-    const { value, inverse } = symbols[c];
+  let prevChar = '';
 
-    if (prev && inverse && inverse.includes(prev)) {
+  // Going in reverse order (end to start).
+  for (let i = s.length - 1; i >= 0; i--) {
+    // Get current char.
+    const char = s[i] as RomanSymbol;
+    // Get the replacement rules.
+    const { value, inverse } = replacements[char];
+
+    // Either add or subtract the value based on previous char.
+    if (prevChar && inverse && inverse.includes(prevChar)) {
       result -= value;
     } else {
       result += value;
     }
 
-    prev = c;
+    // This var is not really needed, we can always read prev value from the string itself.
+    // Just for illustration purposes.
+    prevChar = char;
   }
 
   return result;
