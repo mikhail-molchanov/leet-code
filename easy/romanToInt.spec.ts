8c21d0b81d684e29d8f114510ab7f476c988a1d3
Mikhail Molchanov <mikhail.molchanov@gmail.com>
2023-04-06 15:14:33 +0300

    romanToInt() problem: comments

--- easy/romanToInt.spec.ts
+++ easy/romanToInt.spec.ts
@@ -2,6 +2,7 @@ import { romanToInt } from './romanToInt';
 
 describe('romanToInt', () => {
   test.each([
+    ['', 0],
     ['I', 1],
     ['XIX', 19],
     ['XXVII', 27],
--- easy/romanToInt.ts
+++ easy/romanToInt.ts
@@ -4,6 +4,7 @@
  */
 
 type RomanSymbol = 'I' | 'V' | 'X' | 'L' | 'C' | 'D' | 'M';
+
 type Replacement = {
   /**
    * A number to add to the result when symbol is found.
@@ -11,7 +12,7 @@ type Replacement = {
   value: number;
   /**
    * What symbols if present right before the current one will inverse its "value" to the negative.
-   * E.g. if I appeared after V (going right to left) then it should be subtracted from the result.
+   * E.g. if "I" appeared before 'V' then it should be subtracted from the result.
    */
   inverse?: readonly string[];
 };
@@ -27,25 +28,28 @@ const replacements: Record<RomanSymbol, Replacement> = {
 };
 
 export const romanToInt = (s: string): number => {
+  // Conversion result is gonna be accumulated here.
   let result = 0;
+
+  // Previously read character. This var is not really needed, we can always read prev
+  // value from the string itself. Just for illustration purposes.
   let prevChar = '';
 
   // Going in reverse order (end to start).
   for (let i = s.length - 1; i >= 0; i--) {
-    // Get current char.
+    // Get current char. The assumption is that it's always gonna be a valid Roman symbol.
     const char = s[i] as RomanSymbol;
-    // Get the replacement rules.
+
+    // Get the replacement rule.
     const { value, inverse } = replacements[char];
 
     // Either add or subtract the value based on previous char.
-    if (prevChar && inverse && inverse.includes(prevChar)) {
+    if (prevChar && inverse?.includes(prevChar)) {
       result -= value;
     } else {
       result += value;
     }
 
-    // This var is not really needed, we can always read prev value from the string itself.
-    // Just for illustration purposes.
     prevChar = char;
   }
 
