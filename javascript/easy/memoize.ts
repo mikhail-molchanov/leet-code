24b39c70d68e83b5e4dfc7779a0721a1897dfdcb
Mikhail Molchanov <mikhail.molchanov@gmail.com>
2023-04-14 10:00:42 +0300

    Memoize problem

---
+++ javascript/easy/memoize.ts
@@ -0,0 +1,23 @@
+/**
+ * Problem description:
+ * https://leetcode.com/problems/memoize/
+ */
+
+type Fn = (...params: any) => any;
+
+// The only problem here is how to build params hash to make it fast and memory efficient.
+function memoize(fn: Fn): Fn {
+  // Map between input args hash and function values.
+  const cache = new Map<string, any>();
+
+  return function (...args) {
+    const hash = JSON.stringify(args);
+    if (cache.has(hash)) {
+      return cache.get(hash);
+    }
+
+    const value = fn.apply(null, args);
+    cache.set(hash, value);
+    return value;
+  };
+}
