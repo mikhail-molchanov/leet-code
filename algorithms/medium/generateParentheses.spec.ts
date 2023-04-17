e51b8ab6c01e30e66175619d59d7658bec85b46d
Mikhail Molchanov <mikhail.molchanov@gmail.com>
2023-04-17 17:54:10 +0300

    Minor

--- algorithms/medium/generateParentheses.spec.ts
+++ algorithms/medium/generateParentheses.spec.ts
@@ -27,4 +27,9 @@ describe('Generate Parentheses', () => {
   ])('valid parentheses for n = %p: %p', (n, output) => {
     expect(generateParenthesis(n)).toEqual(output);
   });
+
+  // Just a small test to see how it grows :)
+  // for (let i = 1; i < 10; i++) {
+  //   console.log(i, generateParenthesis(i).length);
+  // }
 });
