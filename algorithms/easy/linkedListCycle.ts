37be5b2900517d9f7641f9d377e714a82ae81095
Mikhail Molchanov <mikhail.molchanov@gmail.com>
2023-04-18 14:11:08 +0300

    Comment

--- algorithms/easy/linkedListCycle.ts
+++ algorithms/easy/linkedListCycle.ts
@@ -1,6 +1,7 @@
 import { ListNode } from '../../shared/utils';
 
 // Simple caching.
+// P.S. Look up constant memory approach (fast and slow pointers), it's really cool.
 export function hasCycle(head: ListNode | null): boolean {
   const nodes = new Set<ListNode>();
 
