aa318c9411c0871b89235777a6e768c37bc16424
Mikhail Molchanov <mikhail.molchanov@gmail.com>
2023-05-24 16:34:35 +0300

    Trapping Rain Water problem (optimized but still slow)

--- algorithms/hard/trappingRainWater.ts
+++ algorithms/hard/trappingRainWater.ts
@@ -7,34 +7,44 @@
 // - Go by rows starting from the bottom, end algorithm when there is no more terrain on level X.
 // - Detect regions that are surrounded by terrain to the left and right and has at least on empty block above
 //   for the water to get in.
+// Optimization 1:
+// - It seems like we don't have to check the "above" blocks meaning that elevation map doesn't have caveats.
+// Optimization 2:
+// - Since terrain can't "levitate" over nothing we can narrow scan area on every iteration.
 export function trap(height: number[]): number {
   let amount = 0;
 
+  let scanFrom = 0;
+  let scanTo = height.length;
+
   // On each iteration we need two rows: current and next one (above).
   const iterate = (index: number): boolean => {
-    let result = false;
-
     let prev = false;
     let start = 0;
-    let hasEmptyAbove = false;
 
-    for (let i = 0; i < height.length; i++) {
+    // console.log('iterate range: ', scanFrom, scanTo);
+
+    let firstTerrainIndex: number | undefined;
+    let lastTerrainIndex: number | undefined;
+
+    for (let i = scanFrom; i < scanTo; i++) {
       const current = height[i] > index;
-      const above = height[i] > index + 1;
 
-      if (above) {
+      if (current) {
+        if (firstTerrainIndex === undefined) {
+          firstTerrainIndex = i;
+        }
         // If next row is not empty we'll need to continue iterations.
-        result = true;
+        lastTerrainIndex = i;
       }
 
       // Terrain.
       if (current) {
-        if (start && hasEmptyAbove) {
+        if (start) {
           amount += i - start;
         }
 
         start = 0;
-        hasEmptyAbove = false;
       }
       // Empty block.
       else {
@@ -42,16 +52,23 @@ export function trap(height: number[]): number {
         if (prev) {
           start = i;
         }
-
-        if (start && !above) {
-          hasEmptyAbove = true;
-        }
       }
 
       prev = current;
     }
 
-    return result;
+    // console.log('scanning range: ', scanFrom, scanTo);
+    if (firstTerrainIndex !== undefined) {
+      scanFrom = firstTerrainIndex;
+      // console.log('updating scanFrom: ', firstTerrainIndex);
+    }
+
+    if (lastTerrainIndex !== undefined) {
+      scanTo = lastTerrainIndex + 1;
+      // console.log('updating scanTo: ', lastTerrainIndex);
+    }
+
+    return !!firstTerrainIndex || !!lastTerrainIndex;
   };
 
   let index = 0;
