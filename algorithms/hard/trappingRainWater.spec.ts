67da180dbcdec25986e462bfc6094a1002053524
Mikhail Molchanov <mikhail.molchanov@gmail.com>
2023-05-24 18:23:23 +0300

    Trapping Rain Water problem (efficient)

--- algorithms/hard/trappingRainWater.spec.ts
+++ algorithms/hard/trappingRainWater.spec.ts
@@ -1,10 +1,11 @@
-import { trap } from './trappingRainWater';
+import { trap, trap2 } from './trappingRainWater';
 
 describe('Trapping Rain Water', () => {
   test.each([
     [[0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1], 6],
     [[4, 2, 0, 3, 2, 5], 9],
   ])('amount of water that can be trapped for %p: %p', (input, result) => {
-    expect(trap(input)).toEqual(result);
+    // expect(trap(input)).toEqual(result);
+    expect(trap2(input)).toEqual(result);
   });
 });
--- algorithms/hard/trappingRainWater.ts
+++ algorithms/hard/trappingRainWater.ts
@@ -78,3 +78,63 @@ export function trap(height: number[]): number {
 
   return amount;
 }
+
+// OK, so after thinking for like an hour I guess I figured something out:
+// - Going row by row as in the previous approach is very inefficient. Image an input of [10000, 0, 10000].
+//   In order to get to the answer we'll have to iterate 10000 times. There must be something more elegant.
+// - Let's go by columns instead of rows.
+// - Now the tricky thing, it's really needs to be visualized but anyway:
+//   - Go from left to right and for every index calculate current maximum value.
+//   - Go from right to left and do the same.
+//   - Now we have 2 lists with maximums in both directions (they will be different, draw a sample graph and you'll get it).
+//   - Now for every index we have 2 maximums, get a minimum value. Here is the catch:
+//     THIS IS GONNA BE THE HIGHEST VALUE THE WATER CAN GO UP FOR THE CURRENT INDEX.
+//   - Now we just have to subtract actual height at this index from the value above.
+//     This is gonna be an amount of water above current index.
+// - Cool thing about it is that we can do all of the above in one pass:
+//   - Go in opposite directions simultaneously
+//   - Once for specific value we have both left/right maximums we can already calculate the water amount =)
+export function trap2(height: number[]): number {
+  let maxLeft = 0;
+  const maxLeftList: number[] = [];
+
+  let maxRight = 0;
+  const maxRightList: number[] = [];
+
+  let amount = 0;
+
+  for (let i = 0; i < height.length; i++) {
+    // Go left to right and fill left => right list.
+    const left = height[i];
+    if (left > maxLeft) {
+      maxLeft = left;
+    }
+
+    maxLeftList[i] = maxLeft;
+
+    // If we already have right to left value for "i" => we can already
+    // calculate the amount and add it to the overall value.
+    if (maxRightList[i]) {
+      const min = Math.min(maxLeftList[i], maxRightList[i]);
+      amount += min - left;
+    }
+
+    // Go right to left at the same time.
+    const j = height.length - i - 1;
+    const right = height[j];
+    if (right > maxRight) {
+      maxRight = right;
+    }
+
+    maxRightList[j] = maxRight;
+
+    // If we already have left to right value for "j" => we can already
+    // calculate the amount and add it to the overall value.
+    if (maxLeftList[j]) {
+      const min = Math.min(maxLeftList[j], maxRightList[j]);
+      amount += min - right;
+    }
+  }
+
+  return amount;
+}
