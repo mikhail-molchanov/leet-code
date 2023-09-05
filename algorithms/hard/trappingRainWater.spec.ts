b92dd939192825850cd54638f1f58d0ce9898a19
Mikhail Molchanov <mikhail.molchanov@gmail.com>
2023-09-05 09:45:16 +0300

    Trapping Rain Water: additional test

--- algorithms/hard/trappingRainWater.spec.ts
+++ algorithms/hard/trappingRainWater.spec.ts
@@ -4,6 +4,8 @@ describe('Trapping Rain Water', () => {
   test.each([
     [[0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1], 6],
     [[4, 2, 0, 3, 2, 5], 9],
+    // This is where version 1 will stuck while version 2 will not loose any performance.
+    [[0, 50000, 0, 100000, 0], 50000],
   ])('amount of water that can be trapped for %p: %p', (input, result) => {
     // expect(trap(input)).toEqual(result);
     expect(trap2(input)).toEqual(result);
